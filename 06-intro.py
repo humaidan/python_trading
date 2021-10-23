b799b58151fee07486ca8525581e2adb1eff88cc
Ali <humaidan@gmail.com>
2021-10-23 13:27:46 +0300

    init

---
+++ 06-intro.py
@@ -0,0 +1,10 @@
+import pandas_datareader.data as pdr
+import datetime as dt
+
+ticker = 'AMZN'
+start_date = dt.date.today() - dt.timedelta(365)
+end_date = dt.date.today()
+
+data = pdr.get_data_yahoo(ticker, start_date, end_date, interval='m')
+
+print(data)
